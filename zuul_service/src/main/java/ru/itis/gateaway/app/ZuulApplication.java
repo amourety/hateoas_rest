7e72f7b683e34fa66e01a7374be0767f26f9de66
amourety <amourety@gmail.com>
2020-05-27 11:38:39 +0300

    Added microservice's architecture for project

---
+++ zuul_service/src/main/java/ru/itis/gateaway/app/ZuulApplication.java
@@ -0,0 +1,16 @@
+package ru.itis.gateaway.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableZuulProxy
+@SpringBootApplication
+@EnableEurekaClient
+public class ZuulApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class);
+    }
+}
